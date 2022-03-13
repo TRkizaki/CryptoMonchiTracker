0d19f24d5e30312c2de390d4c259c4c67b454b6c
TR Kizaki <trkizaki.web@gmail.com>
2022-03-14 03:32:35 +0400

    add giobal market info

---
+++ CryptoMonchi/Services/MarketDataService.swift
@@ -0,0 +1,36 @@
+//
+//  MarketDataService.swift
+//  CryptoMonchi
+//
+//  Created by DJ perrier  on 14/3/2022.
+//
+
+import Foundation
+import Combine
+
+class MarketDataService {
+    
+    @Published var marketData: MarketDataModel? = nil
+    
+    var marketDataSubscription: AnyCancellable?
+   
+    
+    init() {
+        getCoins()
+    }
+    
+    private func getCoins() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/global")
+        else { return }
+        
+        
+        marketDataSubscription = NetworkingManager.download(url: url)//new //followed all logic  from download func by NetworkingManager module
+            .decode(type: GlobalData.self, decoder: JSONDecoder())
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedGlobalData) in //updated
+                self?.marketData = returnedGlobalData.data
+                self?.marketDataSubscription?.cancel()
+                })
+            
+    }
+}
+
