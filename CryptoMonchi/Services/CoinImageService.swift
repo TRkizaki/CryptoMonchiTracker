5cdfac6c61624e68d4008e65b9c2b5a7a81ee294
TR Kizaki <trkizaki.web@gmail.com>
2022-03-13 15:57:55 +0400

    set up FileManager

--- CryptoMonchi/Services/CoinImageService.swift
+++ CryptoMonchi/Services/CoinImageService.swift
@@ -15,25 +15,41 @@ class CoinImageService {//download logic
     
     private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance //new
+    private let folderName = "coin_images"//new
+    private let imageName: String //new
     
     init(coin: CoinModel) {// update
         self.coin = coin
-        getCoinImage()//CoinImagesviewModelにurlstringを担当してもらうからC
-        //oinImageServiceではcoin methodからdataをpickupすることにfocus 
+        self.imageName = coin.id
+        getCoinImage()
+    }
+    
+    private func getCoinImage() {//re-define
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName) {
+            image = savedImage
+            //print("Retrieve image from File Manager!")
+        } else {
+            downloadCoinImage()
+            //print("Downloding Image Now")
+        }
         
     }
     
-    private func getCoinImage() {
+    private func downloadCoinImage() {//update from getCoinImage
+        //print("Downloding Image Now")
         guard let url = URL(string: coin.image) else { return }
         
-        imageSubscription = NetworkingManager.download(url: url)//new //followed all logic  from download func by NetworkingManager module
+        imageSubscription = NetworkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 
                 return UIImage(data: data)
             })
             .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in //updated
-                self?.image = returnedImage
-                self?.imageSubscription?.cancel()
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
                 })
            
         
